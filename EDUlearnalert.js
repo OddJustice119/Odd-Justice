e562316d454b7d08ec0e315786c14f204cd667f3
Matthew Manning <machmanning@gmail.com>
2021-01-08 17:04:31 -0330

    tutorial stuff more

---
+++ EDUlearnalert.js
@@ -0,0 +1,17 @@
+/*This is basically just test code*/
+
+//"use strict"; //This disables classes and modules and probably will not be needed later
+
+alert(' Yo whats up');
+alert('Holy bababa');
+alert(3+7);
+
+testvariable = 'testing, testing?';
+age = 25;
+user= 'John';
+
+
+
+
+alert (testvariable, age, John) //This uses what is contained in testvariable
+
