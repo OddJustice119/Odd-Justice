9eb6787177f9b2347e52cf4b0ca2f093ac6f8201
Matthew Manning <machmanning@gmail.com>
2021-01-15 16:32:49 -0330

    learned a bit more

---
+++ Learning stuff/comparisons.js
@@ -0,0 +1,35 @@
+/*
+In javascript
+
+greater/less than is > or <
+Greater/less than or equal to is >= or <=
+Equals is == (equality test)
+Not equals in Javascript is written as !=
+*/
+
+/* when comparing strings, z>a>Z>A
+  If numbers are strings, the leading digit determines greater (ie '2'>'12')
+*/
+
+
+//&& is AND
+//|| is OR
+//Exclaimation point is NOT
+
+let userName = prompt("who are you?","");
+
+if (userName === 'deathskull2000') {
+    let pass = prompt ('password, Master Burst?');
+}
+
+if (pass === 'fuckkirkstagg') {
+    alert('Welcome! <3');
+}   else if (pass === ''|| pass === null){
+    alert ("cancelled");
+    } else{
+        alert ('Gattling turret initiated');
+    } else if (username === '' || username === null); {
+    alert('goodbye');
+    } else {
+    alert ('please leave');
+    }
